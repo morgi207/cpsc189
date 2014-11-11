b0b10ba9a3dbe99fc5f71df03d9bbde2ae1c67d0
morgi207 <psychoswimmergirl@hotmail.com>
2014-11-10 17:40:09 -0800

    212 last week

--- notebooks/assign5.py
+++ notebooks/assign5.py
@@ -4,6 +4,7 @@ Created on Thu Nov 06 17:29:01 2014
 
 @author: Morgen
 """
+#importing and loading
 import os,site
 currdir=os.getcwd()
 head,tail=os.path.split(currdir)
@@ -13,25 +14,28 @@ import new_walk
 reload(new_walk)
 
 files=new_walk.index('\Users\Morgen\Documents\GitHub\classcode\notebooks\cpsc189\notebooks')
-
+#opening the files
+#finding the name,seze,owner, and date modified
+#spliting the data
 def tree_to_csv(filename,filelist):
     with open(filename,'w') as f:
         #write the header first
         f.write('file_name;owner;size;modified\n')
         for the_line in filelist:
             thelevel=len(the_line[0].split(os.sep))-1
-            f.write("{:s};{:s};{:d};{:d};{;d}\n".format(*the_line,thelevel)
-
+            f.write("{:s};{:s};{:d};{:d};{:d}\n".format(*the_line,thelevel))
+#error here, not sure why?
 tree_to_csv('out.csv',files)
 
 import pandas as pd
 
 data=pd.read_csv('out.csv',sep=';')
 data
-
+#printing the level
+#adding in the indents for each level
 out=data.groupby('thelevel')
-    for thelevel, dflevel in out:
-        print thelevel,dflevel[site].sum()
-            for index, item in dflevel.iterrows():
-                indent=(thelevel-1)*'    '
-                print thelevel, item('filename')
+for thelevel, dflevel in out:
+   print thelevel,dflevel[site].sum()
+   for index, item in dflevel.iterrows():
+       indent=(thelevel-1)*'    '
+       print thelevel, item('filename')
